9a5f303cc85dd8d600a60e0c1ace356d16a07dff
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-05 13:49:58 -0500

    first commit

---
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -0,0 +1,16 @@
+public class MainChallenge {
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score < 5000) {
+            System.out.println("Your score was 5000");
+        } else {
+            System.out.println("Your score less than 5000");
+        }
+
+    }
+}
