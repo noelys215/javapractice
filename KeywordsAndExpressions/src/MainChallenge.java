ebea08c8b7bb0137a3b4ccaa14c27e883de7b3c2
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-05 14:10:34 -0500

    added challenge

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,16 +1,38 @@
 public class MainChallenge {
     public static void main(String[] args) {
-
         boolean gameOver = true;
-        int score = 5000;
+        int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
+        int finalScore = score;
 
-        if (score < 5000) {
-            System.out.println("Your score was 5000");
-        } else {
-            System.out.println("Your score less than 5000");
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Final Score: " + finalScore);
         }
+
+        score = 10_000;
+        levelCompleted = 8;
+        bonus = 200;
+        finalScore = score;
+
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Final Score: " + finalScore);
+        }
+
+//        boolean newGameOver = true;
+//        int newScore = 10000;
+//        int newLevelCompleted = 8;
+//        int newBonus = 200;
+//
+//        int newFinalScore = newScore;
+//
+//        if (newGameOver) {
+//            newFinalScore += (newLevelCompleted * newBonus);
+//            System.out.println("Final Score: " + newFinalScore);
+//        }
 
     }
 }
