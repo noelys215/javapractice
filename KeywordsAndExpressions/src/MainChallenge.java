78dd66511ccc31c6b99c7b593f2fe23556913e7b
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-05 19:17:51 -0500

    updated main challenge

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,15 +1,21 @@
 public class MainChallenge {
     public static void main(String[] args) {
-        calcScore(true, 800, 5, 100);
+        int highScore = calcScore(true, 800, 5, 100);
+
+        System.out.println("1st High Score: " + highScore);
+        System.out.println("2nd High Score: " + calcScore(true, 10_000, 8, 200));
     }
 
-    //METHOD
-    public static void calcScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    /* METHOD */
+    public static int calcScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         int finalScore = score;
 
         if (gameOver) {
             finalScore += (levelCompleted * bonus);
-            System.out.println("Final Score: " + finalScore);
+            finalScore += 1000;
         }
+
+        return finalScore;
     }
+
 }
