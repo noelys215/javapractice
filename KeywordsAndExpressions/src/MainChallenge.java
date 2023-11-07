b23392b3e8ce0e48d5deaaf1cf59cec218d39d99
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-06 22:00:39 -0500

    added challenge 7

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,5 +1,31 @@
 public class MainChallenge {
-    public static boolean hasEqualSum(int num1, int num2, int num3) {
-        return (num1 + num2) == num3;
+    public static boolean hasTeen(int num1, int num2, int num3) {
+        if (num1 >= 13 && num1 <= 19) {
+            return true;
+        } else if (num2 >= 13 && num2 <= 19) {
+            return true;
+        } else if (num3 >= 13 && num3 <= 19) {
+            return true;
+        } else {
+            return false;
+        }
     }
+
+    public static boolean isTeen(int num1) {
+        if (num1 >= 13 && num1 <= 19) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));  // should return true
+        System.out.println(hasTeen(25, 15, 42));  // should return true
+        System.out.println(hasTeen(22, 23, 34)); // should return true
+        System.out.println(isTeen(9)); // should return true
+        System.out.println(isTeen(13)); // should return true
+    }
+
 }
