a720bb81a1c477d8850ac50a2a83ac8360f171f8
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-06 19:35:37 -0500

    added challenge 3

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,23 +1,20 @@
 public class MainChallenge {
 
-    public static long toMilesPerHour(double kilometersPerHour) {
-        if (kilometersPerHour < 0) {
-            return -1;
-        }
-        return Math.round(kilometersPerHour / 1.609);
-    }
-
-    public static void printConversion(double kilometersPerHour) {
-        if (kilometersPerHour < 0) {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
             System.out.println("Invalid Value");
         } else {
-            long milesPerHour = toMilesPerHour(kilometersPerHour);
-            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+            int megabytes = kiloBytes / 1024;
+            int remainingKiloBytes = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megabytes + " MB and " + remainingKiloBytes + " KB");
         }
     }
 
     public static void main(String[] args) {
-        printConversion(95.75); // Should print "95.75 km/h = 60 mi/h".
-        printConversion(-5);    // Should print "Invalid Value".
+        printMegaBytesAndKiloBytes(2500); // Expected output: "2500 KB = 2 MB and 452 KB"
+        printMegaBytesAndKiloBytes(-1024); // Expected output: "Invalid Value"
+        printMegaBytesAndKiloBytes(5000); // Expected output: "5000 KB = 4 MB and 904 KB"
     }
+
+
 }
