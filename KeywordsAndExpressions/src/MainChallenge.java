df877fecfce35c4bc0863b399e51292af60afe6f
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-06 19:53:01 -0500

    added challenge 4

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,19 +1,14 @@
 public class MainChallenge {
 
-    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
-        if (kiloBytes < 0) {
-            System.out.println("Invalid Value");
-        } else {
-            int megabytes = kiloBytes / 1024;
-            int remainingKiloBytes = kiloBytes % 1024;
-            System.out.println(kiloBytes + " KB = " + megabytes + " MB and " + remainingKiloBytes + " KB");
-        }
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        return barking && (hourOfDay < 8 || hourOfDay > 22) && (hourOfDay >= 0 && hourOfDay <= 23);
     }
 
     public static void main(String[] args) {
-        printMegaBytesAndKiloBytes(2500); // Expected output: "2500 KB = 2 MB and 452 KB"
-        printMegaBytesAndKiloBytes(-1024); // Expected output: "Invalid Value"
-        printMegaBytesAndKiloBytes(5000); // Expected output: "5000 KB = 4 MB and 904 KB"
+        System.out.println(shouldWakeUp(true, 1));  // should return true
+        System.out.println(shouldWakeUp(false, 2)); // should return false
+        System.out.println(shouldWakeUp(true, 8));  // should return false
+        System.out.println(shouldWakeUp(true, -1)); // should return false
     }
 
 
