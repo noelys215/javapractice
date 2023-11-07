8e863c786b43e22fb977ab8a02d2a1fbbe20690a
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-06 21:44:12 -0500

    added challenge 5

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,15 +1,5 @@
 public class MainChallenge {
-
-    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
-        return barking && (hourOfDay < 8 || hourOfDay > 22) && (hourOfDay >= 0 && hourOfDay <= 23);
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+        return (int) (num1 * 1000) == (int) (num2 * 1000);
     }
-
-    public static void main(String[] args) {
-        System.out.println(shouldWakeUp(true, 1));  // should return true
-        System.out.println(shouldWakeUp(false, 2)); // should return false
-        System.out.println(shouldWakeUp(true, 8));  // should return false
-        System.out.println(shouldWakeUp(true, -1)); // should return false
-    }
-
-
 }
