0b2cb2ae075a9f7b87a1578be9f79cb68dd1bdcf
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-06 22:02:20 -0500

    updated challenge 7

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,22 +1,10 @@
 public class MainChallenge {
     public static boolean hasTeen(int num1, int num2, int num3) {
-        if (num1 >= 13 && num1 <= 19) {
-            return true;
-        } else if (num2 >= 13 && num2 <= 19) {
-            return true;
-        } else if (num3 >= 13 && num3 <= 19) {
-            return true;
-        } else {
-            return false;
-        }
+        return isTeen(num1) || isTeen(num2) || isTeen(num3);
     }
 
     public static boolean isTeen(int num1) {
-        if (num1 >= 13 && num1 <= 19) {
-            return true;
-        } else {
-            return false;
-        }
+        return (num1 >= 13 && num1 <= 19);
     }
 
 
