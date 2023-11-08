662d084c11e48091a8a691e112c04610541c4d2a
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-07 20:26:39 -0500

    added second method overloading challenge

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,16 +1,21 @@
 public class MainChallenge {
-    public static double convertToCent(int inches) {
-        return inches * 2.54;
+    public static String getDurationString(int seconds) {
+        return seconds < 0 ? "Invalid value" : getDurationString(seconds / 60, seconds % 60);
     }
 
-    public static double convertToCent(int feet, int inches) {
-        return convertToCent(feet * 12 + inches);
+    public static String getDurationString(int minutes, int seconds) {
+        if (minutes < 0 || seconds < 0 || seconds >= 60) {
+            return "Invalid value";
+        }
+        return String.format("%02dh %02dm %02ds", minutes / 60, minutes % 60, seconds);
     }
 
 
     public static void main(String[] args) {
-        System.out.println(convertToCent(68)); // Convert 68 inches to centimeters
-        System.out.println(convertToCent(5, 8)); // Convert 5 feet 8 inches to centimeters
+        System.out.println(getDurationString(65, 45)); // should return "01h 05m 45s"
+        System.out.println(getDurationString(3945));   // should return "01h 05m 45s"
+        System.out.println(getDurationString(61, 0));  // should return "01h 01m 00s"
+        System.out.println(getDurationString(3600));   // should return "01h 00m 00s"
     }
 
 }
