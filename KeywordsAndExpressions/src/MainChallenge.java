8e8beb28d3b2215599cb3ef8b8920a9bdc56fa5f
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-09 16:24:42 -0500

    added challenge 11 update

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,14 +1,9 @@
 public class MainChallenge {
     public static void printEqual(int a, int b, int c) {
-        if (a < 0 || b < 0 || c < 0) {
-            System.out.println("Invalid Value");
-        } else if (a == b && b == c) {
-            System.out.println("All numbers are equal");
-        } else if (a != b && b != c &&  a != c) {
-            System.out.println("All numbers are different");
-        } else {
-            System.out.println("Neither all are equal or different");
-        }
+        System.out.println(a < 0 || b < 0 || c < 0 ? "Invalid Value" :
+                (a == b && b == c) ? "All numbers are equal" :
+                        (a != b && b != c && a != c) ? "All numbers are different" :
+                                "Neither all are equal or different");
     }
 
 
