2126c798a4ad08a10a0db1bafa44be389255d2bb
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-08 21:59:19 -0500

    added challenge 9

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,21 +1,18 @@
 public class MainChallenge {
-    public static String getDurationString(int seconds) {
-        return seconds < 0 ? "Invalid value" : getDurationString(seconds / 60, seconds % 60);
+    public static double area(double radius) {
+        return (radius < 0) ? -1 : radius * (radius * Math.PI);
     }
 
-    public static String getDurationString(int minutes, int seconds) {
-        if (minutes < 0 || seconds < 0 || seconds >= 60) {
-            return "Invalid value";
-        }
-        return String.format("%02dh %02dm %02ds", minutes / 60, minutes % 60, seconds);
+    public static double area(double x, double y) {
+        return (x < 0 || y < 0) ? -1 : x * y;
     }
 
 
     public static void main(String[] args) {
-        System.out.println(getDurationString(65, 45)); // should return "01h 05m 45s"
-        System.out.println(getDurationString(3945));   // should return "01h 05m 45s"
-        System.out.println(getDurationString(61, 0));  // should return "01h 01m 00s"
-        System.out.println(getDurationString(3600));   // should return "01h 00m 00s"
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
     }
 
 }
