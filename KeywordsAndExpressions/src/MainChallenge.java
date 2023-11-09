037b2f3546e0877dc8042919a4dc6bea4e478554
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-09 16:06:51 -0500

    added challenge 10

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,18 +1,15 @@
 public class MainChallenge {
-    public static double area(double radius) {
-        return (radius < 0) ? -1 : radius * (radius * Math.PI);
-    }
-
-    public static double area(double x, double y) {
-        return (x < 0 || y < 0) ? -1 : x * y;
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.printf("%01d min = %01d y and %01d d", minutes, minutes / 525600, (minutes / 1440) % 365);
+        }
     }
 
 
     public static void main(String[] args) {
-        System.out.println(area(5.0));
-        System.out.println(area(-1));
-        System.out.println(area(5.0, 4.0));
-        System.out.println(area(-1.0, 4.0));
+        printYearsAndDays(525600);
     }
 
 }
