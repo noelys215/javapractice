e16eae1063dacee16244f85b52e910e0e9fbfaae
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-09 16:22:30 -0500

    added challenge 11

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,15 +1,22 @@
 public class MainChallenge {
-    public static void printYearsAndDays(long minutes) {
-        if (minutes < 0) {
+    public static void printEqual(int a, int b, int c) {
+        if (a < 0 || b < 0 || c < 0) {
             System.out.println("Invalid Value");
+        } else if (a == b && b == c) {
+            System.out.println("All numbers are equal");
+        } else if (a != b && b != c &&  a != c) {
+            System.out.println("All numbers are different");
         } else {
-            System.out.printf("%d min = %d y and %d d", minutes, minutes / 525600, (minutes / 1440) % 365);
+            System.out.println("Neither all are equal or different");
         }
     }
 
 
     public static void main(String[] args) {
-        printYearsAndDays(525600);
+        printEqual(1, 1, 1); //should print text All numbers are equal
+        printEqual(1, 1, 2); //should print text Neither all are equal or different
+        printEqual(-1, -1, -1); //should print text Invalid Value
+        printEqual(1, 2, 3); //should print text All numbers are different
     }
 
 }
