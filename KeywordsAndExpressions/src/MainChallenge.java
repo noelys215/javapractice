f264b691dbf149e5b62e134045844707909047c1
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-09 16:43:40 -0500

    added challenge 12

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,17 +1,13 @@
 public class MainChallenge {
-    public static void printEqual(int a, int b, int c) {
-        System.out.println(a < 0 || b < 0 || c < 0 ? "Invalid Value" :
-                (a == b && b == c) ? "All numbers are equal" :
-                        (a != b && b != c && a != c) ? "All numbers are different" :
-                                "Neither all are equal or different");
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        int upperLimit = summer ? 45 : 35;
+        return temperature >= 25 && temperature <= upperLimit;
     }
 
-
     public static void main(String[] args) {
-        printEqual(1, 1, 1); //should print text All numbers are equal
-        printEqual(1, 1, 2); //should print text Neither all are equal or different
-        printEqual(-1, -1, -1); //should print text Invalid Value
-        printEqual(1, 2, 3); //should print text All numbers are different
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
     }
 
 }
