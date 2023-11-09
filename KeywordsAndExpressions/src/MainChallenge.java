3fefacc902cb1975bba54e416782eee88d0c1f61
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-09 16:08:24 -0500

    added challenge 10 update

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -3,7 +3,7 @@ public class MainChallenge {
         if (minutes < 0) {
             System.out.println("Invalid Value");
         } else {
-            System.out.printf("%01d min = %01d y and %01d d", minutes, minutes / 525600, (minutes / 1440) % 365);
+            System.out.printf("%d min = %d y and %d d", minutes, minutes / 525600, (minutes / 1440) % 365);
         }
     }
 
