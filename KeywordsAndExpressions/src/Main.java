e749399ec85b45fefd9aacfb19e1819ae1b25f0c
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-09 20:36:37 -0500

    started switch statements

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,17 +1,41 @@
 public class Main {
+
     public static void main(String[] args) {
-        int newScore = calcScore("Asuka", 500);
-        System.out.println("New score is " + newScore);
+
+        char charVal = 'Z';
+        switch (charVal) {
+            case ('A'):
+                System.out.println("A is able");
+                break;
+            case ('B'):
+                System.out.println("B is baker");
+                break;
+            case ('C'):
+                System.out.println("C is charlie");
+                break;
+            case ('D'):
+                System.out.println("D is dog");
+                break;
+            case ('E'):
+                System.out.println("E is easy");
+                break;
+            default:
+                System.out.println("Letter " + charVal + " was not found in statement.");
+                break;
+        }
+
     }
 
-    public static int calcScore(String playerName, int score) {
-        System.out.println("Player " + playerName + " scored " + score + " points.");
-        return score * 1000;
-    }
-
-    public static int calcScore(int score) {
-        System.out.println("Unknown " + " scored " + score + " points.");
-        return score * 1000;
+    public static String getQuarter(String month) {
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> {
+                yield month + " is invalid.";
+            }
+        };
     }
 
 }
