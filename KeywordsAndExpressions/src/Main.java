896b5686af8ae0ac43298ccccab86b84e5092a5d
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-09 20:58:46 -0500

    switch statements examples

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,41 +1,23 @@
 public class Main {
-
     public static void main(String[] args) {
-
-        char charVal = 'Z';
-        switch (charVal) {
-            case ('A'):
-                System.out.println("A is able");
-                break;
-            case ('B'):
-                System.out.println("B is baker");
-                break;
-            case ('C'):
-                System.out.println("C is charlie");
-                break;
-            case ('D'):
-                System.out.println("D is dog");
-                break;
-            case ('E'):
-                System.out.println("E is easy");
-                break;
-            default:
-                System.out.println("Letter " + charVal + " was not found in statement.");
-                break;
-        }
-
+        printDayOfWeek(0);
     }
 
-    public static String getQuarter(String month) {
-        return switch (month) {
-            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
-            case "APRIL", "MAY", "JUNE" -> "2nd";
-            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
-            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
-            default -> {
-                yield month + " is invalid.";
+    public static void printDayOfWeek(int day) {
+        String dayOfWeek = switch (day) {
+            case 0 -> {
+                yield "Sunday";
             }
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "No day found.";
         };
+        System.out.println(dayOfWeek);
     }
+
 
 }
