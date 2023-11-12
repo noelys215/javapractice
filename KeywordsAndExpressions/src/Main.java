61f896b9acea9d6eb92b6455e677fde69a94156c
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-12 13:19:28 -0500

    and loop challenge

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,12 +1,31 @@
 public class Main {
-    public static boolean isLeapYear(int year) {
-        if (year < 1 || year > 9999) {
-            return false;
+
+    public static void main(String[] args) {
+        int primeCount = 0;
+
+        // Loop through a range of numbers (e.g., 1 to 1000)
+        for (int i = 1; i <= 1000; i++) {
+            if (isPrime(i)) {
+                System.out.println(i + " is a prime number");
+                primeCount++;
+
+                // Exit the loop if three prime numbers are found
+                if (primeCount == 3) {
+                    break;
+                }
+            }
         }
-        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 
-
-
-
+    public static boolean isPrime(int wholeNumber) {
+        if (wholeNumber < 2) {
+            return false;
+        }
+        for (int divisor = 2; divisor <= Math.sqrt(wholeNumber); divisor++) {
+            if (wholeNumber % divisor == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
