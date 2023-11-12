9e2081b8b0ca0b5d2de6783015c05b2289bfae5e
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-11 21:05:40 -0500

    added challenge 13

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,22 +1,27 @@
 public class Main {
-    public static void main(String[] args) {
-        printNumberInWord(-1);
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        }
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 
-    public static void printNumberInWord(int number) {
-        System.out.println(switch (number) {
-            case 0 -> "ZERO";
-            case 1 -> "ONE";
-            case 2 -> "TWO";
-            case 3 -> "THREE";
-            case 4 -> "FOUR";
-            case 5 -> "FIVE";
-            case 6 -> "SIX";
-            case 7 -> "SEVEN";
-            case 8 -> "EIGHT";
-            case 9 -> "NINE";
-            default -> "other";
-        });
+    public static int getDaysInMonth(int month, int year) {
+        if (month < 1 || month > 12 || year < 1 || year > 9999) {
+            return -1;
+        }
+
+        switch (month) {
+            case 2: // February
+                return (isLeapYear(year)) ? 29 : 28;
+            case 4: // April
+            case 6: // June
+            case 9: // September
+            case 11: // November
+                return 30;
+            default:
+                return 31;
+        }
     }
 
 
