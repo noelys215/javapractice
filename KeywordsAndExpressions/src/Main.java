d7881423eb0faac8d4041c933fb66e375f71c573
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-13 11:42:11 -0500

    add challenge 22

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,16 +1,13 @@
 public class Main {
-    public static void printFactors(int number) {
-        if (number < 1) {
-            System.out.println("Invalid Value");
-            return;
+    public static int getGreatestCommonDivisor(int first, int second) {
+        if (first < 10 || second < 10) return -1;
+
+
+        int gcd = 1;
+        for (int i = 1; i <= Math.min(first, second); i++) {
+            if (first % i == 0 && second % i == 0) gcd = i;
         }
 
-        for (int i = 1; i <= number; i++) {
-            if (number % i == 0) System.out.println(i);
-        }
-    }
-
-    public static void main(String[] args) {
-        printFactors(6);
+        return gcd;
     }
 }
