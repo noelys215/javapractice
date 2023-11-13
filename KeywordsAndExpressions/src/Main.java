af4e4ae4ce8e8ccef496845ddba588af4005eb06
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-12 19:45:49 -0500

    simplify challenge 16

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,26 +1,15 @@
 public class Main {
-    public static int sumDigits(int number) {
-        if (number < 0) return -1;
+    public static boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reverse = 0;
 
-
-        int sum = 0;
-
-        while (number > 0) {
-            // Extract the least-significant digit
-            int digit = number % 10;
-            sum += digit;
-
-            // Remove the least-significant digit
+        while (number != 0) {
+            int lastDigit = number % 10;
+            reverse = reverse * 10 + lastDigit;
             number /= 10;
         }
-        return sum;
-    }
 
-    public static void main(String[] args) {
-        System.out.println("Sum of the digits in 125 is " + sumDigits(125));
-        System.out.println("Sum of the digits in -125 is " + sumDigits(-125));
-        System.out.println("Sum of the digits in 4 is " + sumDigits(4));
-        System.out.println("Sum of the digits in 4 is " + sumDigits(1000));
+        return originalNumber == reverse;
     }
 
 
