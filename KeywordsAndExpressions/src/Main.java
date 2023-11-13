33dbfb1a96f9ba4c1b9d0cb923a834c7a42293c7
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-12 21:00:32 -0500

    simplify challenge 18

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,15 +1,22 @@
 public class Main {
 
-    public static int sumFirstAndLastDigit(int number) {
+    public static int getEvenDigitSum(int number) {
         if (number < 0) return -1;
 
-        int lastDigit = number % 10;
-        int firstDigit = number;
 
-        while (firstDigit >= 10) firstDigit /= 10;
+        int sum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            if (digit % 2 == 0) {
+                sum += digit;
+            }
+            number /= 10;
+        }
 
-        return firstDigit + lastDigit;
+        return sum;
     }
 
-
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789));
+    }
 }
