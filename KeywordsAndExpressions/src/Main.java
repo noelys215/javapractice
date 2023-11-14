24880a816397c12701f225fc959b266bd52e060e
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-13 21:12:20 -0500

    add challenge 23

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,13 +1,49 @@
 public class Main {
-    public static int getGreatestCommonDivisor(int first, int second) {
-        if (first < 10 || second < 10) return -1;
+    public static void numberToWords(int number) {
+        if (number < 0) {
+            System.out.println("Invalid Value");
+        } else if (number == 0) {
+            System.out.println("Zero");
+        } else {
+            int reversedNumber = reverse(number);
+            int originalDigitCount = getDigitCount(number);
+            int reversedDigitCount = getDigitCount(reversedNumber);
 
+            while (reversedNumber != 0) {
+                int digit = reversedNumber % 10;
+                switch (digit) {
+                    case 0 -> System.out.println("Zero");
+                    case 1 -> System.out.println("One");
+                    case 2 -> System.out.println("Two");
+                    case 3 -> System.out.println("Three");
+                    case 4 -> System.out.println("Four");
+                    case 5 -> System.out.println("Five");
+                    case 6 -> System.out.println("Six");
+                    case 7 -> System.out.println("Seven");
+                    case 8 -> System.out.println("Eight");
+                    case 9 -> System.out.println("Nine");
+                }
+                reversedNumber /= 10;
+            }
 
-        int gcd = 1;
-        for (int i = 1; i <= Math.min(first, second); i++) {
-            if (first % i == 0 && second % i == 0) gcd = i;
+            for (int i = reversedDigitCount; i < originalDigitCount; i++) {
+                System.out.println("Zero");
+            }
         }
+    }
 
-        return gcd;
+    public static int reverse(int number) {
+        int reversed = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+        return reversed;
+    }
+
+    public static int getDigitCount(int number) {
+        if (number < 0) return -1;
+        return number == 0 ? 1 : (int) Math.log10(number) + 1;
     }
 }
