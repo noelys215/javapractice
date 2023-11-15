28f5ab3b6944253959eb5e06102ef03e008753cc
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-14 20:49:30 -0500

    added console input

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -6,8 +6,12 @@ public class Main {
     }
 
     public static String getConsoleInput(int currentYear) {
+        String name = System.console().readLine("Hi, What's your name?");
+        System.out.println("Hi " + name + ", thanks for taking this course! c: ");
+        String dateOfBirth = System.console().readLine("What year were you born?");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
 
-        return "";
+        return "So you're " + age + " years old?";
     }
 
     public static String getScannerInput(int currentYear) {
