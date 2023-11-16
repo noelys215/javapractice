c5a0add2586c1807a06d543a033f73bd984437d6
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-15 21:22:23 -0500

    updated scanner project

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -2,8 +2,8 @@ public class MainChallenge {
     public static int getLargestPrime(int number) {
         if (number <= 1) return -1;
 
-
         int largestPrime = -1;
+
         for (int i = 2; i <= number; i++) {
             while (number % i == 0) {
                 largestPrime = i;
