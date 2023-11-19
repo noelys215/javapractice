a6f65df460db13b2417ea2c730c0a3a8867b344c
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-18 20:04:21 -0500

    fixed challenge

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,20 +1,26 @@
 public class MainChallenge {
-    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
-        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) return -1;
-        double area = width * height;
-        double neededBuckets = Math.ceil(area / areaPerBucket);
-        return (int) (neededBuckets - extraBuckets);
+    public class PaintJob {
+
+        // Private helper method to calculate the bucket count
+        private static int calculateBuckets(double area, double areaPerBucket) {
+            return (int) Math.ceil(area / areaPerBucket);
+        }
+
+        public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+            if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) return -1;
+            return calculateBuckets(width * height, areaPerBucket) - extraBuckets;
+        }
+
+        public static int getBucketCount(double width, double height, double areaPerBucket) {
+            if (width <= 0 || height <= 0 || areaPerBucket <= 0) return -1;
+            return calculateBuckets(width * height, areaPerBucket);
+        }
+
+        public static int getBucketCount(double area, double areaPerBucket) {
+            if (area <= 0 || areaPerBucket <= 0) return -1;
+            return calculateBuckets(area, areaPerBucket);
+        }
     }
 
-    public static int getBucketCount(double width, double height, double areaPerBucket) {
-        if (width <= 0 || height <= 0 || areaPerBucket <= 0) return -1;
-        double area = width * height;
-        return (int) Math.ceil(area / areaPerBucket);
-    }
-
-    public static int getBucketCount(double area, double areaPerBucket) {
-        if (area <= 0 || areaPerBucket <= 0) return -1;
-        return (int) Math.ceil(area / areaPerBucket);
-    }
 
 }
