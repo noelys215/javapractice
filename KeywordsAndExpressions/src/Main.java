221024bf3d50e3964302eb3532ed1bf90ba28f8c
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-19 11:11:16 -0500

    add oop challenge 0

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,56 +1,21 @@
-import java.util.Scanner;
-
 public class Main {
+
     public static void main(String[] args) {
-        int currentYear = 2024;
+        Car car = new Car();
+        car.setMake("Mercedes");
+        car.setModel("AMG G 63");
+        car.setColor("Obsidian Black Metallic");
+        car.setDoors(4);
+        car.setConvertible(false);
+        car.describeCar();
 
-        try {
-            System.out.println(getConsoleInput(currentYear));
-        } catch (NullPointerException e) {
-            System.out.println(getScannerInput(currentYear));
-        }
-    }
-
-    public static String getConsoleInput(int currentYear) {
-        String name = System.console().readLine("Hi, What's your name?");
-        System.out.println("Hi " + name + ", thanks for taking this course! c: ");
-        String dateOfBirth = System.console().readLine("What year were you born?");
-        int age = currentYear - Integer.parseInt(dateOfBirth);
-
-        return "So you're " + age + " years old?";
-    }
-
-    public static String getScannerInput(int currentYear) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Hi, What's your name? ");
-        String name = scanner.nextLine();
-        System.out.println("Hi " + name + ", thanks for taking this course! c: ");
-        System.out.println("What year were you born? ");
-
-        boolean validDOB = false;
-        int age = 0;
-        do {
-            System.out.println("Enter a year of birth >= " + (currentYear - 125) + " and <= " + (currentYear));
-            try {
-                age = checkData(currentYear, scanner.nextLine());
-                validDOB = age < 0 ? false : true;
-            } catch (NumberFormatException badUserData) {
-                System.out.println("Characters not allowed!");
-            }
-        } while (!validDOB);
-
-        return "So you're " + age + " years old?";
-    }
-
-
-    public static int checkData(int currentYear, String dateOfBirth) {
-        int dob = Integer.parseInt(dateOfBirth);
-        int minYear = currentYear - 125;
-
-        if ((dob < minYear) || (dob > currentYear)) return -1;
-
-        return currentYear - dob;
+        Car porsche = new Car();
+        porsche.setMake("Porsche");
+        porsche.setColor("911");
+        porsche.setModel("Piano Black");
+        porsche.setDoors(2);
+        porsche.setConvertible(true);
+        porsche.describeCar();
     }
 
 }
--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,26 +1,10 @@
 public class MainChallenge {
-    public class PaintJob {
+    public static void main(String[] args) {
+        Account shinjisAccount = new Account();
+        shinjisAccount.withdrawFunds(100.00);
+        shinjisAccount.depositFunds(250.00);
+        shinjisAccount.withdrawFunds(50.00);
 
-        // Private helper method to calculate the bucket count
-        private static int calculateBuckets(double area, double areaPerBucket) {
-            return (int) Math.ceil(area / areaPerBucket);
-        }
-
-        public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
-            if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) return -1;
-            return calculateBuckets(width * height, areaPerBucket) - extraBuckets;
-        }
-
-        public static int getBucketCount(double width, double height, double areaPerBucket) {
-            if (width <= 0 || height <= 0 || areaPerBucket <= 0) return -1;
-            return calculateBuckets(width * height, areaPerBucket);
-        }
-
-        public static int getBucketCount(double area, double areaPerBucket) {
-            if (area <= 0 || areaPerBucket <= 0) return -1;
-            return calculateBuckets(area, areaPerBucket);
-        }
+        shinjisAccount.withdrawFunds(200);
     }
-
-
 }
