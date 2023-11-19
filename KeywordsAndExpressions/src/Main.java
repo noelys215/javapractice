4d5ddc23bc47cdc4398fbefc60c34e76ee7f9b31
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-19 14:48:49 -0500

    added POJO example

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,15 +1,19 @@
 public class Main {
-
     public static void main(String[] args) {
-        Customer customer = new Customer("Marie", 1000.0, "marie@nerv.net");
+        for (int i = 1; i <= 5; i++) {
+            Student s = new Student("S92300" + i,
+                    switch (i) {
+                        case 1 -> "Rei";
+                        case 2 -> "Asuka";
+                        case 3 -> "Mari";
+                        case 4 -> "Shinji";
+                        case 5 -> "Kaworu";
+                        default -> "Anon";
+                    },
+                    "03/30/2001",
+                    "Eva Piloting");
 
-        System.out.println(customer.getName());
-        System.out.println(customer.getCreditLimit());
-        System.out.println(customer.getEmail());
-
-        Customer secondCustomer = new Customer();
-        System.out.println(secondCustomer.getName());
-        System.out.println(secondCustomer.getCreditLimit());
-        System.out.println(secondCustomer.getEmail());
+            System.out.println(s);
+        }
     }
 }
