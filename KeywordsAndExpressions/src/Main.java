ed42e088574b60cb63eec1914f994fd62359ffb8
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-20 20:19:13 -0500

    added string formats

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,22 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Employee asuka = new Employee("Asuka", "12/05/2005", "01/02/2021");
-        System.out.println(asuka);
-        System.out.println("Age: " + asuka.getAge());
-        System.out.println("Pay: " + asuka.collectPay());
+        String textBlock = """
+                Print a Bulleted List:\s
+                    • First Point
+                    • Sub Point
+                """;
+        //
+        int age = 35;
+//        System.out.printf("Your age is %d%n", age);
+        int yearOfBirth = 2023 - age;
+//        System.out.printf("Age: %d %nYOB: %d", age, yearOfBirth);
+        for (int i = 1; i <= 100000; i *= 10) System.out.printf("Printing %6d %n", i);
 
-        SalariedEmployee rei = new SalariedEmployee("Rei", "11/15/2004", "01/02/2020", 45000);
-        System.out.println(rei);
-        System.out.println("Rei's Pay: $" + rei.collectPay());
+        String formattedString = String.format("Your age is %d", age);
+        System.out.println(formattedString);
 
-        rei.retire();
-        System.out.println("Rei's Pension: $" + rei.collectPay());
-
-        HourlyEmployee mari = new HourlyEmployee("Mari", "04/06/2000", "01/02/2010", 45);
-        System.out.println(mari);
-        System.out.println("Mari's Pay: $" + mari.collectPay());
-        System.out.println("Mari's Holiday Pay: $" + mari.getDoublePay());
+        formattedString = "Your age is %d".formatted(age);
+        System.out.println(formattedString);
     }
-
-
 }
