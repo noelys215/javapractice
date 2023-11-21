411ae4c58dfc1503c8d52c23cd79deb5d45ce63f
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-20 21:05:24 -0500

    added string builder methods

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,21 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        String textBlock = """
-                Print a Bulleted List:\s
-                    • First Point
-                    • Sub Point
-                """;
-        //
-        int age = 35;
-//        System.out.printf("Your age is %d%n", age);
-        int yearOfBirth = 2023 - age;
-//        System.out.printf("Age: %d %nYOB: %d", age, yearOfBirth);
-        for (int i = 1; i <= 100000; i *= 10) System.out.printf("Printing %6d %n", i);
+        String helloWorld = "Hello" + " World";
+        StringBuilder helloWorldBuilder = new StringBuilder("Hello" + " World");
 
-        String formattedString = String.format("Your age is %d", age);
-        System.out.println(formattedString);
+        printInformation(helloWorld);
+        printInformation(helloWorldBuilder);
+    }
 
-        formattedString = "Your age is %d".formatted(age);
-        System.out.println(formattedString);
+    public static void printInformation(String string) {
+        System.out.println("String: " + string);
+        System.out.println("Length: " + string.length());
+    }
+
+    public static void printInformation(StringBuilder builder) {
+        System.out.println("StringBuilder: " + builder);
+        System.out.println("Length: " + builder.length());
     }
 }
