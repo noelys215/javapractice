0212511a8e4fdbe999dc91919e932ff4e0bfa2fb
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-23 18:17:01 -0500

    added smartKitchen challenge

--- KeywordsAndExpressions/src/MainChallenge.java
+++ KeywordsAndExpressions/src/MainChallenge.java
@@ -1,5 +1,18 @@
 public class MainChallenge {
     public static void main(String[] args) {
+        SmartKitchen kitchen = new SmartKitchen();
 
+        kitchen.setKitchenState(true, true, true);
+        kitchen.doKitchenWork();
+
+        /*
+        kitchen.getDishWasher().setHasWorkToDo(true);
+        kitchen.getIceBox().setHasWorkToDo(true);
+        kitchen.getBrewMaster().setHasWorkToDo(true);
+
+        kitchen.getDishWasher().doDishes();
+        kitchen.getIceBox().orderFood();
+        kitchen.getBrewMaster().brewCoffee();
+        */
     }
 }
