e468661dc1d6bcfa83fa6799b07668d33bc5e36a
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-23 11:20:31 -0500

    started PC project

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,19 +1,3 @@
 public class Main {
-    public static void main(String[] args) {
-        String helloWorld = "Hello" + " World";
-        StringBuilder helloWorldBuilder = new StringBuilder("Hello" + " World");
 
-        printInformation(helloWorld);
-        printInformation(helloWorldBuilder);
-    }
-
-    public static void printInformation(String string) {
-        System.out.println("String: " + string);
-        System.out.println("Length: " + string.length());
-    }
-
-    public static void printInformation(StringBuilder builder) {
-        System.out.println("StringBuilder: " + builder);
-        System.out.println("Length: " + builder.length());
-    }
 }
