1ffffa6c05bfe90b8469ebbf0ceafc1c1558a048
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-29 20:45:08 -0500

    added arrays binary search

--- KeywordsAndExpressions/src/dev/lpa/Main.java
+++ KeywordsAndExpressions/src/dev/lpa/Main.java
@@ -11,7 +11,6 @@ public class Main {
         System.out.println(Arrays.toString(firstArray));
         Arrays.sort(firstArray);
         System.out.println(Arrays.toString(firstArray));
-        */
         int[] secondArray = getRandomArray(10);
         System.out.println(Arrays.toString(secondArray));
         Arrays.fill(secondArray, 5);
@@ -23,7 +22,14 @@ public class Main {
 
         int[] fourthArray = Arrays.copyOf(thirdArray, thirdArray.length);
         System.out.println(Arrays.toString(fourthArray));
-
+        */
+        String[] sArray = {"Able", "Jane", "Mark", "Ralph", "David"};
+        Arrays.sort(sArray);
+        System.out.println(Arrays.toString(sArray));
+        if (Arrays.binarySearch(sArray, "Mark") >= 0) System.out.println("Mark Found");
+        int[] s1 = {1, 2, 3, 4, 5};
+        int[] s2 = {1, 2, 3, 4, 5, 0};
+        System.out.println(Arrays.equals(s1, s2) ? "Arrays Equal" : "Arrays Not Equal");
     }
 
     private static int[] getRandomArray(int len) {
