ca869faabfc6419499d7ac64058722bf227ef115
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-29 21:34:16 -0500

    added java references

--- KeywordsAndExpressions/src/dev/lpa/Main.java
+++ KeywordsAndExpressions/src/dev/lpa/Main.java
@@ -1,42 +1,23 @@
 package dev.lpa;
 
 import java.util.Arrays;
-import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        /*
-        int[] firstArray = getRandomArray(10);
-        System.out.println(Arrays.toString(firstArray));
-        Arrays.sort(firstArray);
-        System.out.println(Arrays.toString(firstArray));
-        int[] secondArray = getRandomArray(10);
-        System.out.println(Arrays.toString(secondArray));
-        Arrays.fill(secondArray, 5);
-        System.out.println(Arrays.toString(secondArray));
-        System.out.println(" ");
+        int[] myIntArray = new int[5];
+        int[] anotherArray = myIntArray;
 
-        int[] thirdArray = getRandomArray(10);
-        System.out.println(Arrays.toString(thirdArray));
+        System.out.println("myIntArray: " + Arrays.toString(myIntArray));
+        System.out.println("anotherArray: " + Arrays.toString(anotherArray));
 
-        int[] fourthArray = Arrays.copyOf(thirdArray, thirdArray.length);
-        System.out.println(Arrays.toString(fourthArray));
-        */
-        String[] sArray = {"Able", "Jane", "Mark", "Ralph", "David"};
-        Arrays.sort(sArray);
-        System.out.println(Arrays.toString(sArray));
-        if (Arrays.binarySearch(sArray, "Mark") >= 0) System.out.println("Mark Found");
-        int[] s1 = {1, 2, 3, 4, 5};
-        int[] s2 = {1, 2, 3, 4, 5, 0};
-        System.out.println(Arrays.equals(s1, s2) ? "Arrays Equal" : "Arrays Not Equal");
+        anotherArray[0] = 1;
+        modifyArray(myIntArray);
+        System.out.println("Altered myIntArray: " + Arrays.toString(myIntArray));
+        System.out.println("Altered anotherArray: " + Arrays.toString(anotherArray));
     }
 
-    private static int[] getRandomArray(int len) {
-        Random random = new Random();
-        int[] newInt = new int[len];
-
-        for (int i = 0; i < len; i++) newInt[i] = random.nextInt(100);
-        return newInt;
+    private static void modifyArray(int[] array) {
+        array[1] = 2;
     }
 }
