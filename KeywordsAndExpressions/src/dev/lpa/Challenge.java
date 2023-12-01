c3525652d91d2ff041b4d885fab21534a33f4603
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-11-30 21:02:57 -0500

    added array challenge

--- KeywordsAndExpressions/src/dev/lpa/Challenge.java
+++ KeywordsAndExpressions/src/dev/lpa/Challenge.java
@@ -1,43 +1,34 @@
 package dev.lpa;
 
 import java.util.Arrays;
-import java.util.Random;
+import java.util.Scanner;
 
 public class Challenge {
     public static void main(String[] args) {
-        int[] unsortedArray = getRandomArray(5);
-        System.out.println(Arrays.toString(unsortedArray));
+        int[] returnArray = readIntegers();
+        System.out.println(Arrays.toString(returnArray));
 
-        int[] sortedArray = sortIntegers(new int[]{7, 30, 35});
-        System.out.println(Arrays.toString(sortedArray));
+        int returnedMin = findMin(returnArray);
+        System.out.println("MIN: " + returnedMin);
     }
 
-    public static int[] getRandomArray(int len) {
-        Random random = new Random();
-        int[] randomArray = new int[len];
-        for (int i = 0; i < len; i++) randomArray[i] = random.nextInt(1000);
-        return randomArray;
-    }
+    private static int[] readIntegers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Integers:");
+        String input = scanner.nextLine();
 
-    private static int[] sortIntegers(int[] array) {
-        System.out.println(Arrays.toString(array));
-        int[] sortedArray = Arrays.copyOf(array, array.length);
-        boolean flag = true;
-        int temp;
-        while (flag) {
-            flag = false;
-            for (int i = 0; i < sortedArray.length - 1; i++) {
-                if (sortedArray[i] < sortedArray[i + 1]) {
-                    temp = sortedArray[i];
-                    sortedArray[i] = sortedArray[i + 1];
-                    sortedArray[i + 1] = temp;
-                    flag = true;
-                    System.out.println("------>" + Arrays.toString(sortedArray));
-                }
-            }
-            System.out.println("---->" + Arrays.toString(sortedArray));
+        String[] splits = input.split(",");
+        int[] values = new int[splits.length];
+        for (int i = 0; i < splits.length; i++) {
+            values[i] = Integer.parseInt(splits[i].trim());
         }
-        return sortedArray;
+        return values;
+    }
+
+    private static int findMin(int[] array) {
+        int min = Integer.MAX_VALUE;
+        for (int el : array) if (el < min) min = el;
+        return min;
     }
 
 }
--- KeywordsAndExpressions/src/dev/lpa/Main.java
+++ KeywordsAndExpressions/src/dev/lpa/Main.java
@@ -1,23 +1,9 @@
 package dev.lpa;
 
-import java.util.Arrays;
-
 public class Main {
+    public static void main(String... args) {
 
-    public static void main(String[] args) {
-        int[] myIntArray = new int[5];
-        int[] anotherArray = myIntArray;
-
-        System.out.println("myIntArray: " + Arrays.toString(myIntArray));
-        System.out.println("anotherArray: " + Arrays.toString(anotherArray));
-
-        anotherArray[0] = 1;
-        modifyArray(myIntArray);
-        System.out.println("Altered myIntArray: " + Arrays.toString(myIntArray));
-        System.out.println("Altered anotherArray: " + Arrays.toString(anotherArray));
     }
 
-    private static void modifyArray(int[] array) {
-        array[1] = 2;
-    }
+
 }
