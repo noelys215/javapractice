a798cad0a98e41474c4ace150492aef75ea188f9
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-12-02 19:14:09 -0500

    added reverse challenge

--- KeywordsAndExpressions/src/dev/lpa/Challenge.java
+++ KeywordsAndExpressions/src/dev/lpa/Challenge.java
@@ -3,14 +3,9 @@ package dev.lpa;
 import java.util.Arrays;
 
 public class Challenge {
-    public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
-        System.out.println("Array before reverse: " + Arrays.toString(array));
-        reverse(array);
-        System.out.println("Array after reverse: " + Arrays.toString(array));
-    }
-
     private static void reverse(int[] array) {
+        System.out.println("Array = " + Arrays.toString(array));
+
         int maxIndex = array.length - 1;
         int midpoint = array.length / 2;
         for (int i = 0; i < midpoint; i++) {
@@ -18,6 +13,8 @@ public class Challenge {
             array[i] = array[maxIndex - i];
             array[maxIndex - i] = temp;
         }
+
+        System.out.println("Reversed array = " + Arrays.toString(array));
     }
 
 
