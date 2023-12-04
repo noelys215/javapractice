5cf9958cc1b1ecf6239bc1a477248779848686ce
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-12-03 21:20:09 -0500

    finished ArrayLists part1

--- KeywordsAndExpressions/src/dev/lpa/Challenge.java
+++ KeywordsAndExpressions/src/dev/lpa/Challenge.java
@@ -1,21 +1,57 @@
 package dev.lpa;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class Challenge {
-    private static void reverse(int[] array) {
-        System.out.println("Array = " + Arrays.toString(array));
+    private static Scanner scanner = new Scanner(System.in);
 
-        int maxIndex = array.length - 1;
-        int midpoint = array.length / 2;
-        for (int i = 0; i < midpoint; i++) {
-            int temp = array[i];
-            array[i] = array[maxIndex - i];
-            array[maxIndex - i] = temp;
+    public static void main(String[] args) {
+        boolean flag = true;
+        ArrayList<String> groceries = new ArrayList<>();
+
+        while (flag) {
+            printActions();
+            switch (Integer.parseInt(scanner.nextLine())) {
+                case 1 -> addItems(groceries);
+                case 2 -> removeItems(groceries);
+                default -> flag = false;
+            }
+            groceries.sort(Comparator.naturalOrder());
+            System.out.println(groceries);
         }
-
-        System.out.println("Reversed array = " + Arrays.toString(array));
     }
 
+    private static void addItems(ArrayList<String> groceries) {
+        System.out.println("Add item(s):");
+        String[] items = scanner.nextLine().split(",");
+        for (String i : items) {
+            String trimmed = i.trim();
+            if (!groceries.contains(trimmed)) groceries.add(trimmed);
+        }
+    }
 
+    private static void removeItems(ArrayList<String> groceries) {
+        System.out.println("Remove item(s):");
+        String[] items = scanner.nextLine().split(",");
+        for (String i : items) {
+            String trimmed = i.trim();
+            groceries.remove(trimmed);
+        }
+    }
+
+    private static void printActions() {
+        String textBlock = """
+                Available actions:
+                                
+                0 - to shutdown
+                                
+                1 - to add item(s) to list (comma delimited list)
+                                
+                2 - to remove any items (comma delimited list)
+                                
+                Enter a number for which action you want to do:""";
+        System.out.print(textBlock + " ");
+    }
 }
--- KeywordsAndExpressions/src/dev/lpa/Main.java
+++ KeywordsAndExpressions/src/dev/lpa/Main.java
@@ -1,39 +1,24 @@
 package dev.lpa;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-
-record GroceryItem(String name, String type, int count) {
-    public GroceryItem(String name) {
-        this(name, "DAIRY", 1);
-    }
-
-
-    @Override
-    public String toString() {
-        return String.format("%d %s in %s", count, name.toUpperCase(), type);
-    }
-}
+import java.util.Comparator;
+import java.util.List;
 
 public class Main {
     public static void main(String... args) {
-        GroceryItem[] groceryArray = new GroceryItem[3];
-        groceryArray[0] = new GroceryItem("milk");
-        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
-        groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
 
-        System.out.println(Arrays.toString(groceryArray));
+        String[] originalArray = new String[]{"First", "Second", "Third"};
+        var originalList = Arrays.asList(originalArray);
 
-        ArrayList objectList = new ArrayList();
-        objectList.add(new GroceryItem("Butter"));
-        objectList.add("Yogurt");
+        originalList.set(0, "one");
+        System.out.println("List: " + originalList);
+        System.out.println("Array: " + Arrays.toString(originalArray));
 
-        ArrayList<GroceryItem> groceryList = new ArrayList<>();
-        groceryList.add(new GroceryItem("Butter"));
-        groceryList.add(new GroceryItem("Milk"));
-        groceryList.add(new GroceryItem("kale", "PRODUCE", 5));
-        groceryList.add(0, new GroceryItem("apples", "PRODUCE", 6));
-        System.out.println(groceryList);
+        originalList.sort(Comparator.naturalOrder());
+        System.out.println("Array: " + Arrays.toString(originalArray));
+
+        List<String> newList = Arrays.asList("Sat", "Sun", "Mon");
+        System.out.println(newList);
     }
 
 }
