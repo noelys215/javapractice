5d5ea2fdefe98f924e7a0c287c219ff8d4de16eb
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-12-06 21:22:41 -0500

    started LinkedList methods

--- KeywordsAndExpressions/src/dev/lpa/Main.java
+++ KeywordsAndExpressions/src/dev/lpa/Main.java
@@ -1,24 +1,56 @@
 package dev.lpa;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String... args) {
+//        LinkedList<String> placesToVisit = new LinkedList<>();
+        var placesToVisit = new LinkedList<String>();
+        placesToVisit.add("Sao Paulo");
+        placesToVisit.add(0, "Rio");
+        System.out.println(placesToVisit);
 
-        String[] originalArray = new String[]{"First", "Second", "Third"};
-        var originalList = Arrays.asList(originalArray);
+        addMoreElements(placesToVisit);
+        System.out.println(placesToVisit);
 
-        originalList.set(0, "one");
-        System.out.println("List: " + originalList);
-        System.out.println("Array: " + Arrays.toString(originalArray));
-
-        originalList.sort(Comparator.naturalOrder());
-        System.out.println("Array: " + Arrays.toString(originalArray));
-
-        List<String> newList = Arrays.asList("Sat", "Sun", "Mon");
-        System.out.println(newList);
+        removeElements(placesToVisit);
+        System.out.println(placesToVisit);
     }
 
+    private static void addMoreElements(LinkedList<String> list) {
+        list.addFirst("Asuka");
+        list.addLast("Shinji");
+        // Queue Methods
+        list.offer("Fortaleza");
+        list.offerFirst("Curitiba");
+        list.offerLast("Porto Alegre");
+        // Stack Methods
+        list.push("Campinas");
+    }
+
+    public static void removeElements(LinkedList<String> list) {
+        list.remove(4);
+        list.remove("Rio");
+
+        System.out.println(list);
+        String s1 = list.remove();
+        System.out.println(s1 + " removed");
+        String s2 = list.removeFirst();
+        System.out.println(s2 + " removed");
+        // Queue?Deque poll methods
+        String p1 = list.poll(); // removes first element
+        System.out.println(p1 + " removed");
+        String p2 = list.pollFirst(); // removes first element
+        System.out.println(p2 + " removed");
+        String p3 = list.pollLast(); // removes last element
+        System.out.println(p3 + " removed");
+        //
+        list.push("Campinas");
+        list.push("Curitiba");
+        list.push("Porto Alegre");
+        System.out.println(list);
+
+        String p4 = list.pop(); // removes first element
+        System.out.println(p4 + " removed");
+    }
 }
