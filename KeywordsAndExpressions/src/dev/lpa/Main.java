4ed5488067bb89b136df80288898d6c3706a0e5e
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-12-08 19:54:29 -0500

    finished linked list challenge

--- KeywordsAndExpressions/src/dev/lpa/Main.java
+++ KeywordsAndExpressions/src/dev/lpa/Main.java
@@ -1,9 +1,10 @@
 package dev.lpa;
 
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 record Place(String name, int distance) {
-
     @Override
     public String toString() {
         return String.format("%s (%d)", name, distance);
@@ -13,39 +14,58 @@ record Place(String name, int distance) {
 public class Main {
     public static void main(String... args) {
         LinkedList<Place> placesToVisit = new LinkedList<>();
-        Place adelaide = new Place("Adelaide", 1374);
-        addPlace(placesToVisit, adelaide);
-        addPlace(placesToVisit, new Place("adelaide", 1374));
+        addPlace(placesToVisit, new Place("Adelaide", 1374));
         addPlace(placesToVisit, new Place("Brisbane", 917));
         addPlace(placesToVisit, new Place("Perth", 3923));
         addPlace(placesToVisit, new Place("Alice Springs", 2771));
         addPlace(placesToVisit, new Place("Darwin", 3972));
         addPlace(placesToVisit, new Place("Melbourne", 877));
-
         placesToVisit.addFirst(new Place("Sydney", 0));
-        System.out.println(placesToVisit);
+
+        var iterator = placesToVisit.listIterator();
+        Scanner scanner = new Scanner(System.in);
+        boolean forward = true;
+
+        printMenu();
+        String menuItem;
+        while (!(menuItem = scanner.nextLine().toUpperCase()).equals("Q")) {
+            switch (menuItem) {
+                case "F":
+                    if (!forward && iterator.hasNext()) iterator.next();
+                    forward = true;
+                    System.out.println(iterator.hasNext() ? iterator.next() : "Final destination reached");
+                    break;
+                case "B":
+                    if (forward && iterator.hasPrevious()) iterator.previous();
+                    forward = false;
+                    System.out.println(iterator.hasPrevious() ? iterator.previous() : "At the starting point");
+                    break;
+                case "M":
+                    printMenu();
+                    break;
+                case "L":
+                    System.out.println(placesToVisit);
+                    break;
+            }
+        }
     }
 
     private static void addPlace(LinkedList<Place> list, Place place) {
-        if (list.contains(place)) {
+        if (list.stream().noneMatch(p -> p.name().equalsIgnoreCase(place.name()))) {
+            list.add(place);
+            list.sort(Comparator.comparingInt(Place::distance));
+        } else {
             System.out.println("Dupe Found: " + place);
-            return;
         }
+    }
 
-        for (Place p : list) {
-            if (p.name().equalsIgnoreCase(place.name())) {
-                System.out.println("Dupe Found: " + place);
-                return;
-            }
-        }
-        int matchedIndex = 0;
-        for (var listPlace : list) {
-            if (place.distance() < listPlace.distance()) {
-                list.add(matchedIndex, place);
-                return;
-            }
-            matchedIndex++;
-        }
-        list.add(place);
+    private static void printMenu() {
+        System.out.println("""
+                Available actions (select word or letter):
+                (F)orward
+                (B)ackwards
+                (L)ist Places
+                (M)enu
+                (Q)uit""");
     }
 }
