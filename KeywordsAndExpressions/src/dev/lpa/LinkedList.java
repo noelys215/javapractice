ec3948ebec40e08e954dac2b32682a642a54ea84
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-12-15 20:07:00 -0500

    add remove first node func

--- KeywordsAndExpressions/src/dev/lpa/LinkedList.java
+++ KeywordsAndExpressions/src/dev/lpa/LinkedList.java
@@ -81,6 +81,16 @@ public class LinkedList {
         return temp;
     }
 
+    public Node removeFirst() {
+        if (length == 0) return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+        if (length == 0) tail = null;
+        return temp;
+    }
+
     public void prepend(int value) {
     }
 
