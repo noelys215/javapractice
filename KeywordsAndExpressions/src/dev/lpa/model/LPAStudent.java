7af1a990e0c38f4f12969afa2259fa070508139f
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-12-19 16:33:07 -0500

    competed genetrics prohect

--- KeywordsAndExpressions/src/dev/lpa/model/LPAStudent.java
+++ KeywordsAndExpressions/src/dev/lpa/model/LPAStudent.java
@@ -15,5 +15,13 @@ public class LPAStudent extends Student {
     public double getPercentComplete() {
         return percentComplete;
     }
+
+    @Override
+    public boolean matchFieldValue(String fieldName, String value) {
+        if (fieldName.equalsIgnoreCase("percentComplete"))
+            return percentComplete <= Integer.parseInt(value);
+
+        return super.matchFieldValue(fieldName, value);
+    }
 }
 
--- KeywordsAndExpressions/src/dev/util/QueryList.java
+++ KeywordsAndExpressions/src/dev/util/QueryList.java
@@ -1,19 +1,30 @@
 package dev.util;
 
 
+import dev.lpa.model.Student;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class QueryList<T extends QueryItem> {
-    private List<T> items;
+public class QueryList<T extends Student & QueryItem> extends ArrayList<T> {
 
-    public QueryList(List<T> items) {
-        this.items = items;
+    public QueryList() {
+
     }
 
-    public List<T> getMatches(String field, String value) {
+    public QueryList(List<T> items) {
+        super(items);
+    }
+
+    public static <T extends QueryItem> List<T> getMatches(List<T> items, String field, String value) {
         List<T> matches = new ArrayList<>();
         for (var item : items) if (item.matchFieldValue(field, value)) matches.add(item);
+        return matches;
+    }
+
+    public QueryList<T> getMatches(String field, String value) {
+        QueryList<T> matches = new QueryList<>();
+        for (var item : this) if (item.matchFieldValue(field, value)) matches.add(item);
         return matches;
     }
 }
