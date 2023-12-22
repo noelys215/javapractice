9f32b2765a1a52621db836b97b48db11f09915f3
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-12-22 18:38:53 -0500

    added more lambda challenges

--- KeywordsAndExpressions/src/dev/lpa/Main.java
+++ KeywordsAndExpressions/src/dev/lpa/Main.java
@@ -1,19 +1,46 @@
 package dev.lpa;
 
-import java.util.function.Function;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
 
+    private static Random random = new Random();
+
 
     public static void main(String[] args) {
+        String[] names = {"Anna", "Bob", "Carole", "David", "Ed", "Ava", "Fred", "Gary"};
 
+        Arrays.setAll(names, i -> names[i].toUpperCase());
+        System.out.println("-> Uppercase");
+        System.out.println(Arrays.toString(names));
+
+        List<String> backedByArray = Arrays.asList(names);
+
+        backedByArray.replaceAll(s -> s += " " + getRandomChar('A', 'D') + ".");
+        System.out.println("-> Random Initial");
+        System.out.println(Arrays.toString(names));
+
+        backedByArray.replaceAll(s -> s += " " + getReversedName(s.split(" ")[0]));
+        System.out.println("-> Reverse Name as Last Name");
+        Arrays.asList(names).forEach(s -> System.out.println(s));
+
+        List<String> newList = new ArrayList<>(List.of(names));
+        newList.removeIf(s -> s.substring(0, s.indexOf(" ")).equals(
+                s.substring(s.lastIndexOf(" ") + 1)
+        ));
+        System.out.println("\n");
+        newList.forEach(s -> System.out.println(s));
     }
 
+    public static char getRandomChar(char startChar, char endChar) {
+        return (char) random.nextInt((int) startChar, (int) endChar + 1);
+    }
 
-    Function<String, String> everySecondChar = source -> {
-        StringBuilder returnVal = new StringBuilder();
-        for (int i = 0; i < source.length(); i++) if (i % 2 == 1) returnVal.append(source.charAt(i));
-        return returnVal.toString();
-    };
+    private static String getReversedName(String firstName) {
+        return new StringBuilder(firstName).reverse().toString();
+    }
 
 }
