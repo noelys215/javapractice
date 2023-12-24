a120510aa9b362cfe0932d7fabcaea5069761182
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-12-23 21:36:27 -0500

    added set examples

--- KeywordsAndExpressions/src/dev/lpa/Main.java
+++ KeywordsAndExpressions/src/dev/lpa/Main.java
@@ -1,12 +1,38 @@
 package dev.lpa;
 
 
-import dev.lpa.games.poker.PokerGame;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        PokerGame fiveCardDraw = new PokerGame(4, 5);
-        fiveCardDraw.startPlay();
+        List<Contact> emails = ContactData.getData("email");
+        List<Contact> phones = ContactData.getData("phone");
+        printData("Phone List", phones);
+        printData("Email List", emails);
 
+        Set<Contact> emailContacts = new HashSet<>(emails);
+        Set<Contact> phoneContacts = new HashSet<>(phones);
+
+        printData("Phone Contacts", phoneContacts);
+        printData("Email Contacts", emailContacts);
+        System.out.println();
+        int index = emails.indexOf(new Contact("Robin Hood"));
+        Contact robinHood = emails.get(index);
+        robinHood.addEmail("Sherwood Forest");
+        robinHood.addEmail("Sherwood Forest");
+        robinHood.replaceEmailIfExists("RHood@sherwoodforest.com", "RHood@sherwoodforest.edu");
+        System.out.println(robinHood);
+
+
+    }
+
+    public static void printData(String header, Collection<Contact> contacts) {
+        System.out.println("----------------------------------------------");
+        System.out.println(header);
+        System.out.println("----------------------------------------------");
+        contacts.forEach(System.out::println);
     }
 }
--- KeywordsAndExpressions/src/dev/lpa/games/GameController.java
+++
@@ -1,7 +0,0 @@
-package dev.lpa.games;
-
-public class GameController {
-    public static void main(String[] args) {
-
-    }
-}
