f26eb0d10a6904446de21651c3783ebfcd3fd45b
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-12-24 08:52:54 -0500

    added set examples

--- KeywordsAndExpressions/src/dev/lpa/Main.java
+++ KeywordsAndExpressions/src/dev/lpa/Main.java
@@ -10,23 +10,52 @@ public class Main {
     public static void main(String[] args) {
         List<Contact> emails = ContactData.getData("email");
         List<Contact> phones = ContactData.getData("phone");
-        printData("Phone List", phones);
-        printData("Email List", emails);
+//        printData("Phone List", phones);
+//        printData("Email List", emails);
 
         Set<Contact> emailContacts = new HashSet<>(emails);
         Set<Contact> phoneContacts = new HashSet<>(phones);
 
-        printData("Phone Contacts", phoneContacts);
-        printData("Email Contacts", emailContacts);
-        System.out.println();
+//        printData("Phone Contacts", phoneContacts);
+//        printData("Email Contacts", emailContacts);
         int index = emails.indexOf(new Contact("Robin Hood"));
         Contact robinHood = emails.get(index);
         robinHood.addEmail("Sherwood Forest");
         robinHood.addEmail("Sherwood Forest");
         robinHood.replaceEmailIfExists("RHood@sherwoodforest.com", "RHood@sherwoodforest.edu");
-        System.out.println(robinHood);
+
+        Set<Contact> unionAB = new HashSet<>();
+        unionAB.addAll(emailContacts);
+        unionAB.addAll(phoneContacts);
+        printData("(A ∪ B) Union of Emails (A) with phone (B)", unionAB);
 
 
+        Set<Contact> intersectAB = new HashSet<>(emailContacts);
+        intersectAB.retainAll(phoneContacts);
+        printData("(A ∩ B) Intersect Phones (B) and Emails (A)", intersectAB);
+
+
+        Set<Contact> intersectBA = new HashSet<>(phoneContacts);
+        intersectBA.retainAll(emailContacts);
+        printData("(B ∩ A) Intersect Emails (A) and phone (B)", intersectBA);
+
+
+        Set<Contact> AMinusB = new HashSet<>(emailContacts);
+        AMinusB.removeAll(phoneContacts);
+        printData("(A - B) Emails (A) - Phones (B)", AMinusB);
+
+
+        Set<Contact> BMinusA = new HashSet<>(phoneContacts);
+        BMinusA.removeAll(emailContacts);
+        printData("(B - A) Phones (B) - Emails (A)", BMinusA);
+
+        Set<Contact> symmetricDiff = new HashSet<>(AMinusB);
+        symmetricDiff.addAll(BMinusA);
+        printData("Symmetric Difference: Phones & Emails", symmetricDiff);
+
+        Set<Contact> symmetricDiff2 = new HashSet<>(unionAB);
+        symmetricDiff2.removeAll(intersectAB);
+        printData("Symmetric Difference: phones & emails", symmetricDiff2);
     }
 
     public static void printData(String header, Collection<Contact> contacts) {
